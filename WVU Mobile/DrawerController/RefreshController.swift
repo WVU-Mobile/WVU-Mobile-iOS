071c5575eae65a7d049cafedf8c28d3851aaddcb
Ricky Deal <rainerdeal@gmail.com>
2015-02-23 11:17:35 -0500

    Just removed changes for Refresh Controller

---
+++ WVU Mobile/DrawerController/RefreshController.swift
@@ -0,0 +1,137 @@
+//
+//  RefreshController.swift
+//  WVU Mobile
+//
+//  Created by Richard Deal on 2/19/15.
+//  Copyright (c) 2015 WVUMobile. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RefreshController: CenterViewController {
+    var PRTView: PRTViewController!
+    var refreshControl: UIRefreshControl!
+    var refreshLoadingView: UIView!
+    var refreshColorView: UIView!
+    var compass_background: UIImageView!
+    var compass_spinner: UIImageView!
+    var isRefreshIconsOverlap: Bool!
+    var isRefreshAnimating: Bool!
+    
+    
+    override func viewDidLoad() {
+        self.setupRefreshControl()
+        super.viewDidLoad()
+    }
+    
+    func setupRefreshControl() {
+        
+        // Programmatically inserting a UIRefreshControl
+        self.refreshControl = UIRefreshControl()
+        
+        // Setup the loading view, which will hold the moving graphics
+        self.refreshLoadingView = UIView(frame: self.refreshControl!.bounds)
+        self.refreshLoadingView.backgroundColor = UIColor.clearColor()
+        
+        // Setup the color view, which will display the rainbowed background
+        self.refreshColorView = UIView(frame: self.refreshControl!.bounds)
+        self.refreshColorView.backgroundColor = UIColor.clearColor()
+        self.refreshColorView.alpha = 0.30
+        
+        // Create the graphic image views
+        compass_background = UIImageView(image: UIImage(named: "mount.png"))
+        self.compass_spinner = UIImageView(image: UIImage(named: "mount.png"))
+        
+        // Add the graphics to the loading view
+        self.refreshLoadingView.addSubview(self.compass_background)
+        self.refreshLoadingView.addSubview(self.compass_spinner)
+        
+        // Clip so the graphics don't stick out
+        self.refreshLoadingView.clipsToBounds = true;
+        
+        // Hide the original spinner icon
+        self.refreshControl!.tintColor = UIColor.clearColor()
+        
+        // Add the loading and colors views to our refresh control
+        self.refreshControl!.addSubview(self.refreshColorView)
+        self.refreshControl!.addSubview(self.refreshLoadingView)
+        
+        // Initalize flags
+        self.isRefreshIconsOverlap = false;
+        self.isRefreshAnimating = false;
+        
+        // When activated, invoke our refresh function
+        self.refreshControl?.addTarget(self, action: "refresh", forControlEvents: UIControlEvents.ValueChanged)
+    }
+    
+    func animateRefreshView() {
+        
+        // Background color to loop through for our color view
+        var colorArray = [UIColor.redColor(), UIColor.blueColor(), UIColor.purpleColor(), UIColor.cyanColor(), UIColor.orangeColor(), UIColor.magentaColor()]
+        
+        // In Swift, static variables must be members of a struct or class
+        struct ColorIndex {
+            static var colorIndex = 0
+        }
+        
+        // Flag that we are animating
+        self.isRefreshAnimating = true;
+        
+        UIView.animateWithDuration(
+            Double(0.3),
+            delay: Double(0.0),
+            options: UIViewAnimationOptions.CurveLinear,
+            animations: {
+                // Rotate the spinner by M_PI_2 = PI/2 = 90 degrees
+                self.compass_spinner.transform = CGAffineTransformRotate(self.compass_spinner.transform, CGFloat(M_PI_2))
+                
+                // Change the background color
+                self.refreshColorView!.backgroundColor = colorArray[ColorIndex.colorIndex]
+                ColorIndex.colorIndex = (ColorIndex.colorIndex + 1) % colorArray.count
+            },
+            completion: { finished in
+                // If still refreshing, keep spinning, else reset
+                if (self.refreshControl!.refreshing) {
+                    self.animateRefreshView()
+                }
+                else {
+                    self.resetAnimation()
+                }
+            }
+        )
+    }
+    
+    func resetAnimation() {
+        // AH
+    }
+    
+    func refresh(){
+        var delayInSeconds = 3.0
+        var popTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delayInSeconds * Double(NSEC_PER_SEC)))
+        dispatch_after(popTime, dispatch_get_main_queue()) { () -> Void in
+            // When done requesting/reloading/processing invoke endRefreshing, to close the control
+            self.refreshControl!.endRefreshing()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override init() {
+        super.init()
+        self.restorationIdentifier = "RefreshController"
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        self.restorationIdentifier = "RefreshController"
+    }
+    
+    func setupNotifications(){
+        var prtDown = UILocalNotification()
+        
+    }
+}
